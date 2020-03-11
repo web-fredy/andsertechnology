98c37127d0d0e59bf091fd4100514f63923ac515
Fredy Gildardo Garcia Mendoza <fredy.ggm15@gmail.com>
2020-03-10 21:33:15 -0600

    Mejoras generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
