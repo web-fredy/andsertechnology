e914627b7afc3529fe020f127074bbac7faf7f8f
Fredy Gildardo <fredy.ggm15@gmail.com>
2020-03-19 13:41:37 -0600

    Mejoras generales

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MenuComponent } from '../../shared/menu/menu.component';
-import { FooterComponent } from '../../shared/footer/footer.component';
 
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    MenuComponent,
-    FooterComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
